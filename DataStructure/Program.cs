42a724c0d6e698b6c9c3635f4e074829aa6ac89e
bi27ar <bi27ar@FOT-CELLC04>
2024-09-26 12:58:19 +0100

    Add project files.

---
+++ DataStructure/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie deadpool3;
+            deadpool3.title = "Deadpool & Wolverine";
+            deadpool3.releaseyear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
+
+            Console.WriteLine(deadpool3.title + "(" + deadpool3.releaseyear + ") rated" + deadpool3.rating);
+
+            Console.ReadKey();
+            Console.WriteLine("\n\n");
+
+            List<TVShows> shows = new List<TVShows>();
+            shows.Add(new TVShows("the Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVShows("The Boys", 2019, 7.99, MovieRating._u));
+            shows.Add(new TVShows("Thor", 2009, 12.45, MovieRating._u));
+
+            for (int i = 0; i < shows.Count; i++)
+            {
+                Console.WriteLine(shows[i].ToString());
+            }   
+            Console.ReadKey();
+        }
+    }
+
+    public enum MovieRating
+    {
+        _u,
+        _12a,
+        _12,
+        _15,
+        _18
+    }
+    public struct Movie
+    {
+        public string title;
+        public int releaseyear;
+        public double price;
+        public MovieRating rating;
+
+    }
+}
---
+++ DataStructure/TVShows.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    internal class TVShows
+    {
+        public string title = "untitled";
+        public int releaseyear;
+        public double price;
+        public MovieRating rating;
+
+        public TVShows(string title, int releaseyear, double price, MovieRating rating)
+        {
+            this.title = title;
+            this.releaseyear = releaseyear;
+            this.price = price;
+            this.rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return title + "(" + releaseyear + ") Rated " + "...£" + price.ToString("0.00");
+        }
+
+    }
+}
